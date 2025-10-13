ffc82f4a7bc35677f425b453729564ddae4b1263
shivalaya-ji <shivalayanka87@gmail.com>
2025-10-13 19:31:31 +0100

    commit

--- Fibonacci Sequence 1.py
+++ Fibonacci Sequence 1.py
@@ -9,11 +9,11 @@ def recur_fibo(n: int) -> int:
     return recur_fibo(n - 1) + recur_fibo(n - 2)
 
 
-if __name__ == "__main__":
-    nterms = int(input("How many terms? "))
-    if nterms <= 0:
-        print("Please enter a positive integer")
-    else:
-        print("Fibonacci sequence:")
-        for i in range(nterms):
-            print(recur_fibo(i))
+# if __name__ == "__main__":
+#     nterms = int(input("How many terms? "))
+#     if nterms <= 0:
+#         print("Please enter a positive integer")
+#     else:
+#         print("Fibonacci sequence:")
+#         for i in range(nterms):
+#             print(recur_fibo(i))
